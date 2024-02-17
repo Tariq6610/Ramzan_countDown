711bdf326e06682e35858fa74b20dfc50cb49833
Tariq6610 <115495245+Tariq6610@users.noreply.github.com>
2024-02-17 09:59:16 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let days = document.querySelector("#days");
+let hours = document.querySelector("#hours");
+let minits = document.querySelector("#minits");
+let seconds = document.querySelector("#seconds");
+
+setInterval( function(){
+    let ramadan = new Date("11 march 2024 19:00:00").getTime()
+    let currentDate = new Date().getTime();
+
+    let timeRemaining = ramadan - currentDate;
+    let day = Math.floor( timeRemaining / (1000*60*60*24));
+    let hrs = Math.floor( (timeRemaining % (1000*60*60*24))/(1000*60*60));
+    let min = Math.floor( (timeRemaining % (1000*60*60))/(1000*60));
+    let sec = Math.floor( (timeRemaining % (1000*60))/(1000));
+    
+    days.innerHTML = day;
+    hours.innerHTML = hrs;
+    minits.innerHTML = min;
+    seconds.innerHTML = sec;
+
+
+},500)
